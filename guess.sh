0ff0edfd8a5d9c11463a13998a1acdf4f96a4657
EthemCuhadar <iethemcuhadar@gmail.com>
2022-07-07 14:10:35 +0300

    Game implementation is ready

--- guess.sh
+++ guess.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 echo "Welcome to the Guess A Number game..."
+
+number=$(( $RANDOM % 100 + 1 ))
+
+echo "Let's guess a number between 1 and 100"
+
+guess=0
+
+while [ "0$guess" -ne $number ]; do
+	read guess
+	[ "0$guess" -lt $number ] && echo "Too low"
+	[ "0$guess" -gt $number ] && echo "Too high"
+done
+
+echo "Gotcha!"
+
+exit 0
